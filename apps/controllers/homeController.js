f2c6eb930f33b6b2e57c9a5ed6f0e4b9935e6918
Thanh Chung <nguyenthanhchungfit@gmail.com>
2018-06-16 22:02:58 +0700

    Cập nhật thông báo error cho login

--- apps/controllers/homeController.js
+++ apps/controllers/homeController.js
@@ -118,10 +118,10 @@ exports.get_login = function(req, res){
                 res.redirect("/manager");
             }
         }else{
-            res.render("login");
+            res.render("login", {data: {}});
         }
     }else{
-        res.render("login");
+        res.render("login", {data: {}});
     } 
 }
 
@@ -130,11 +130,11 @@ exports.post_login = function(req, res){
     var err = "";
     if(body.username == ""){
         err += "Chưa nhập username</br>";
-        res.send(err);
+        res.render("login", {data:{error : err}});
     }else{
         if(body.password == ""){
             err += "Chưa nhập password</br>";
-            res.send(err);
+            res.render("login", {data:{error : err}});
         }else{
             customerModel.isValidAccount(body.username, body.password).then(function(isValidCustomer){
                 if(isValidCustomer){
@@ -153,7 +153,7 @@ exports.post_login = function(req, res){
                                     res.redirect("/manager");
                                 }else{
                                     err = "Tài khoản hoặc mật khẩu sai";
-                                    res.send(err);
+                                    res.render("login", {data:{error : err}});
                                 }
                             });
                         }
