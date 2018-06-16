7161fe28f5e4d043533fda3690b0f7b5278c63b6
Thanh Chung <nguyenthanhchungfit@gmail.com>
2018-06-16 09:39:46 +0700

    thêm file insert tài khoản

---
+++ update_DB/insertKH_NV_QL.sql
@@ -0,0 +1,12 @@
+INSERT INTO khach_hang(username, password, ho_ten, email, so_dien_thoai, gioi_tinh, dia_chi, image_url, diem_tich_luy)
+VALUES ('ntchung', '$2b$05$9GuDHWsw.K3ZvvVg0ABnre/wsicX4xJU2pkGh4QEfTcrohtTMIAvO', n'Nguyễn Thành Chung', 'nguyenthanhchungfit@gmail.com',
+'01629009486', 0, n'TPHCM', 'ntchung.jpg', 0);
+
+
+INSERT INTO nhan_vien(username, password, ho_ten, email, so_dien_thoai, gioi_tinh, dia_chi, image_url, luong)
+VALUES ('nttuananh', '$2b$05$cJKqG9Eb.MXiFIGM5SJma.JePqG5sl6qERhGHbphmHrQeF1eDabs2', n'Nguyễn Trần Tuấn Anh', 'nttuananh@gmai.com',
+'0903039182', 1, n'TPHCM', 'nttuananh.jpg', 7000000);
+
+INSERT INTO quan_ly(username, password, ho_ten, email, so_dien_thoai, gioi_tinh, dia_chi, image_url, luong)
+VALUES ('nthanhchung', '$2b$05$HcaX9XjYmR3bwqrHODkuVOGx0rODfel1.OHgXqL3gilucor7CAUt6', n'Nguyễn Thành Chung', 'ntcfit@gmail.com',
+'01629009487', 0, n'TPHCM', 'nthanhchung.jpg', 10000000);
