fb71a5194dde25ed6c8da98ad37eba1fa9965949
Thanh Chung <nguyenthanhchungfit@gmail.com>
2018-06-17 21:54:40 +0700

    Xây dựng chức năng update thông tin customer

--- apps/controllers/customerController.js
+++ apps/controllers/customerController.js
@@ -11,9 +11,7 @@ exports.dashboard = function(req, res){
         res.redirect("../login");
     }else{
         customerModel.getImageUrlByUsername(user.username).then(function(userimg){
-            console.log(userimg);
             var image_link = "../static/imgs/users/customer/" + userimg.user.image_url;
-            console.log(image_link);
             res.render("customer/index", {data : {user : user, image_link : image_link}});
         });
     }
@@ -34,7 +32,7 @@ exports.information = function(req, res){
             }
             res.render("customer/information", {data : {user : user.user, image_link : image_link}});
         }).catch(function(err){
-
+            res.send(err);
         });    
     }
 }
@@ -45,9 +43,8 @@ exports.edit_information = function(req, res){
         res.redirect("../login");
     }else{
         customerModel.getInforCustomerByUsername(user_session.username).then(function(user){
-            console.log(user);
             var select = {nam : '', nu : ''};
-            if(user.gioi_tinh == 0){
+            if(user.user.gioi_tinh == 0){
                 select.nam = 'selected';
             }else{
                 select.nu = 'selected';
@@ -61,12 +58,84 @@ exports.edit_information = function(req, res){
 }
 
 exports.edit_information_post = function(req, res){
-    var user = req.session.user;
-    if(!user){
+    var user_session = req.session.user;
+    if(!user_session){
         res.redirect("../login");
     }else{
-        var image_link = "../static/imgs/users/customer/" + user.username + ".jpg";
-        res.render("customer/edit_information", {data : {user : user, image_link : image_link}}); 
+        var user_body = {
+            email : req.body.email.trim(),
+            ho_ten : req.body.ho_ten.trim(),
+            gioi_tinh : req.body.gioi_tinh,
+            so_dien_thoai : req.body.so_dien_thoai.trim(),
+            dia_chi : req.body.dia_chi.trim()
+        };
+
+        if(user_body.gioi_tinh == "nam"){
+            user_body.gioi_tinh = 0;
+        }else{
+            user_body.gioi_tinh = 1;
+        }
+
+        customerModel.getInforCustomerByUsername(user_session.username).then(function(user){
+            var user_real = user.user;
+            var image_link = "../static/imgs/users/customer/" + user.user.image_url;
+
+            var select = {nam : '', nu : ''};
+            if(user.user.gioi_tinh == 0){
+                select.nam = 'selected';
+            }else{
+                select.nu = 'selected';
+            }
+
+            var success = "Cập nhật thành công!";
+            var esuccess = "Cập nhật thất bại!";
+
+            if(user_real.email == user_body.email && user_real.ho_ten == user_body.ho_ten
+                && user_real.gioi_tinh == user_body.gioi_tinh && user_real.so_dien_thoai ==
+                user_body.so_dien_thoai && user_real.dia_chi == user_real.dia_chi){
+                    
+                    res.render("customer/edit_information", {data : {user : user.user, image_link : image_link, select : select, success : success}}); 
+            }else{
+                var err = "";
+                if(user_real.email != user_body.email){
+                    if(customerModel.checkEmailIsExistedWithUsername(user_real.username, user_body.email)){
+                        err += "<br/>Email này đã tồn tại!";
+                    }
+                }
+
+                if(user_real.so_dien_thoai != user_body.so_dien_thoai){
+                    if(customerModel.checkPhoneNumberIsExistedWithUsername(user_real.username, user_body.so_dien_thoai)){
+                        err += "<br/>Số điện thoại này đã tồn tại!";
+                    }
+                }
+
+                if(err != ""){
+                    res.render("customer/edit_information", {data : {user : user.user, image_link : image_link, select : select, esuccess : esuccess}}); 
+                }else{
+                    customerModel.updateNewInformation(user_real.username, user_body).then(function(isSuccess){
+                        user.user.email = user_body.email;
+                        user.user.ho_ten = user_body.ho_ten;
+                        user.user.gioi_tinh = user_body.gioi_tinh;
+                        user.user.so_dien_thoai = user_body.so_dien_thoai;
+                        user.user.dia_chi = user_body.dia_chi;
+
+                        console.log("user new", user.user);
+
+                        if(user.user.gioi_tinh == 0){
+                            select.nam = 'selected';
+                        }else{
+                            select.nu = 'selected';
+                        }
+                        res.render("customer/edit_information", {data : {user : user.user, image_link : image_link, select : select, success : success}});
+                    }).catch(function(err){
+                        res.send(err);
+                    })
+                }
+
+            }        
+        }).catch(function(err){
+            res.send(err);
+        });    
     }
 }
 
